69317613a54466b37a9b59a3ff197d9f3f7e8786
David Agor <agordavid14@gmail.com>
2024-08-31 14:50:06 +0100

    dynamically render lists

--- react-project/src/components/ListGroup.tsx
+++ react-project/src/components/ListGroup.tsx
@@ -3,18 +3,17 @@
 
 function ListGroup()
 {
+
+    // render items dynamically
+    const cities = ['Lagos', 'Abuja', 'Rivers', 'Jos', 'Calabar'];
+
+   
     return (
         // <Fragment>
       <>
         <h1>Research Organizations in Nigeria</h1>
         <ul className="mt-5 text-danger list-group">
-          <li className="list-group-item">Geopoll Research</li>
-          <li className="list-group-item">Ipsos Nigeria</li>
-          <li className="list-group-item">Nelson and Nelson</li>
-          <li className="list-group-item">Peiring Consulting</li>
-          <li className="list-group-item">RMS-TNS Research</li>
-          <li className="list-group-item">QualiQuant</li>
-          <li className="list-group-item">Autowave Integrated Services</li>
+          {  cities.map((item) => <li className="list-group-item">{ item}</li>) }
         </ul>
       </>
       // </Fragment>
