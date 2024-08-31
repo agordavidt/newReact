5ece3458ce938b85477de88e530b542a5da75b9a
David Agor <agordavid14@gmail.com>
2024-08-31 13:11:43 +0100

    react components

---
+++ react-project/src/Salvation.tsx
@@ -0,0 +1,13 @@
+
+
+// function for savation
+
+function Salvation(){
+    // JSX: JavaScript XML
+
+    const name = 'Joel';
+    return <h1>{ true? 'Hello': 'Goodbye' } Geopoll Interview call</h1>
+};
+
+
+export default Salvation;
