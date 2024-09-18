c5b78e973cdfd3423487e5049774a70f4e69317e
David Agor <agordavid14@gmail.com>
2024-09-18 19:00:25 +0100

    props and useStates

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,6 +1,24 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import Board from "./components/Square";
+
+
+
+export default function App(){
+  return (
+    <>
+      <Board />
+     
+     
+    </>
+  );
 }
 
 
+
 // export default Square;
+
+
+/*
+The export JavaScript keyword makes this function accessible outside of this file. 
+The default keyword tells other files using your code that it’s the main function in your file.
+
+*/
